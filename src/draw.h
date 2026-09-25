42cd7b7cd91f6b4a8b28d45e0185c54b8d2d7a38
AceAryan <acearyan8@gmail.com>
2026-09-26 01:54:54 +0530

    feat: add line handling for all octants and triangle outline

--- src/draw.h
+++ src/draw.h
@@ -4,17 +4,41 @@
 void drawLine(Framebuffer &f, int x0, int y0, int x1, int y1, int r, int g, int b){
     // using Bresenham's line drawing algorithm
 
+    bool steep = abs(y1-y0) > abs(x1-x0);
+    if(steep){
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+    }
+    if(x0 > x1){
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
     int delx = x1 - x0;
     int dely = y1 - y0;
+
+    int yi = 1;
+    if(dely < 0){
+        yi = -1;
+        dely = -dely;
+    }
+
     int p = 2*dely - delx;
 
     while(x0 <= x1){
-        f.setPixel(x0, y0, r, g, b);
+        if(steep) f.setPixel(y0, x0, r, g, b);
+        else f.setPixel(x0, y0, r, g, b);
         x0++;
         if(p < 0) p += 2*dely;
         else{
-            y0++;
+            y0 += yi;
             p += 2*dely - 2*delx;
         }
     }
+}
+
+void drawTriangle(Framebuffer &f, int x0, int y0, int x1, int y1, int x2, int y2, int r, int g, int b){
+    drawLine(f, x0, y0, x1, y1, r, g, b);
+    drawLine(f, x1, y1, x2, y2, r, g, b);
+    drawLine(f, x2, y2, x0, y0, r, g, b);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,19 @@ int main(){
 
     Framebuffer f(800, 600);
 
-    for(int i=0; i<800; i++){
-        for(int j=0; j<600; j++){
+    for(int j=0; j<600; j++){
+        for(int i=0; i<800; i++){
             f.setPixel(i, j , (1 - (float)i/800)*255, ((float)i/800)*255, 0);
         }
     }
 
-    drawLine(f, 40, 40, 400, 400, 0, 0, 255);
+    // drawLine(f, 100, 100, 500, 100, 255, 255, 255);     
+    // drawLine(f, 500, 100, 100, 500, 255, 255, 255);       
+    // drawLine(f, 100, 500, 100, 100, 255, 255, 255); 
+    
+    drawTriangle(f, 100, 100, 500, 100, 100, 500, 255, 255, 255);
 
-    f.writePPM("image3.ppm");
+    f.writePPM("image5.ppm");
     
     return 0;
 }
